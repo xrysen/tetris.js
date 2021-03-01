5bbffb0c302d0e1eeca93308207ae9d99e7e3a91
Sean Oyler <xrysen@gmail.com>
2021-03-01 13:41:51 -0800

    Adds game over condition

--- tetris.js
+++ tetris.js
@@ -140,6 +140,10 @@ const playerReset = () => {
   player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
   player.pos.y = 0;
   player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+
+  if (collide(arena, player)) {
+    arena.forEach(row => row.fill(0));
+  }
 }
 
 const rotate = (matrix, direction) => {
