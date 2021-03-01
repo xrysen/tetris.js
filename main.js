31c510e38a14b258d4d383e35f80ec8c07522244
Sean Oyler <xrysen@gmail.com>
2021-03-01 11:14:35 -0800

    Adds play area

---
+++ main.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+class Board {
+
+  reset() {
+    this.grid = this.getEmptyBoard();
+  }
+
+  getEmptyBoard() {
+    return Array.from (
+      {length: ROWS}, () => Array(COLS).fill(0)
+    );
+  }
+}
+
+let board = new Board();
+
+const play = () => {
+  board.reset();
+  console.table(board.grid);
+}
