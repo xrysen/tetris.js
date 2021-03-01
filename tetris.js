22e984e1adbeee11c31c5aacd8cfd50033913afe
Sean Oyler <xrysen@gmail.com>
2021-03-01 13:54:14 -0800

    Adds line clear

--- tetris.js
+++ tetris.js
@@ -3,6 +3,19 @@ const context = canvas.getContext("2d");
 
 context.scale(20, 20);
 
+const checkForLines = () => {
+  outer: for (let y = arena.length - 1; y > 0; y--) {
+    for (let x = 0; x < arena[y].length; x++) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    y++;
+  }
+}
+
 const collide = (arena, player) => {
   const m = player.matrix;
   const o = player.pos;
@@ -109,6 +122,7 @@ const playerDrop = () => {
     player.pos.y--;
     merge(arena, player);
     playerReset();
+    checkForLines();
   }
   dropCounter = 0;
 };
