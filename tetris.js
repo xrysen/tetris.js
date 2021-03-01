f1d6bd5aa2ef957c0da64fe97a67a50e12dd55d9
Sean Oyler <xrysen@gmail.com>
2021-03-01 12:55:58 -0800

    Adds collision

--- tetris.js
+++ tetris.js
@@ -1,5 +1,103 @@
 const canvas = document.getElementById("tetris");
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.scale(20, 20);
+
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+];
+
+const collide = (arena, player) => {
+  const m = player.matrix;
+  const o = player.pos;
+  for (let y = 0; y < m.length; y++) {
+    for (let x = 0; x < m[y].length; x++) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x] !== 0))   {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const createMatrix = (width, height) => {
+  const matrix = [];
+  while (height--) {
+    matrix.push(new Array(width).fill(0));
+  }
+  return matrix;
+};
+
+const draw = () => {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  drawMatrix(player.matrix, player.pos);
+};
+
+const drawMatrix = (matrix, offset) => {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = "red";
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
+  });
+};
+
+const merge = (arena, player) => {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+};
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+
+const playerDrop = () => {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+};
+
+const update = (time = 0) => {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    playerDrop();
+  }
+  draw();
+  requestAnimationFrame(update);
+};
+
+const arena = createMatrix(12, 20);
+
+const player = {
+  pos: { x: 5, y: 5 },
+  matrix: matrix,
+};
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    player.pos.x--;
+  } else if (event.key === "ArrowRight") {
+    player.pos.x++;
+  } else if (event.key === "ArrowDown") {
+    playerDrop();
+  }
+});
+
+update();
